0dd3729bbb6ca77283ecdf481f72b5d4a3cb354e
Amol Harsh <amolharsh123@gmail.com>
2024-12-11 16:46:52 -0800

    Teams page

---
+++ assets/js/team_list.js
@@ -0,0 +1,79 @@
+class TeamList extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    renderCategory(categoryName, members) {
+        return (
+            <div key={categoryName} className="team-category">
+                {/* Add text-center and padding using inline styles */}
+                <h3
+                    className="category-title text-center"
+                    style={{
+                        paddingTop: "50px", // Add padding before the title
+                        paddingBottom: "10px", // Add padding after the title
+                    }}
+                >
+                    {this.formatCategoryName(categoryName)}
+                </h3>
+                <div className="row gy-5 justify-content-center">
+                    {Object.entries(members).map(([name, details], index) => (
+                        <div
+                            className="col-lg-4 col-md-6 member"
+                            data-aos="fade-up"
+                            data-aos-delay={(index + 1) * 100}
+                            key={name}
+                        >
+                            <div className="member-img">
+                                <img
+                                    src={details.img || 'assets/img/default-avatar.png'}
+                                    className="img-fluid"
+                                    alt={name}
+                                    style={{
+                                        width: "250px",
+                                        height: "250px",
+                                        objectFit: "cover",
+                                        borderRadius: "25%",
+                                        backgroundColor: "#f0f0f0",
+                                    }}
+                                />
+                            </div>
+                            <div className="member-info text-center">
+                                <h4>{name}</h4>
+                                <span>{details.role}</span>
+                                {details.affiliation && <p>{details.affiliation}</p>}
+                                {details.email && (
+                                    <p>Email: <a href={`mailto:${details.email}`}>{details.email}</a></p>
+                                )}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    }
+
+    // Format category names to make them more readable
+    formatCategoryName(categoryName) {
+        return categoryName
+            .replace(/_/g, ' ') // Replace underscores with spaces
+            .replace(/\b\w/g, char => char.toUpperCase()); // Capitalize first letter of each word
+    }
+
+    // Render the full team list
+    renderTeamList() {
+        return Object.entries(team_list).map(([category, members]) =>
+            this.renderCategory(category, members)
+        );
+    }
+
+    // Render method
+    render() {
+        return <div className="container">{this.renderTeamList()}</div>;
+    }
+}
+
+// Mount the TeamList component to the DOM
+ReactDOM.render(
+    <TeamList />,
+    document.getElementById('team') // Ensure this matches the placeholder ID in your HTML
+);
---
+++ data/members.js
@@ -0,0 +1,94 @@
+var team_list = {
+    principal_investigator: {
+        "Tauhidur Rahman": {
+            img: "https://datascience.ucsd.edu/wp-content/uploads/2023/01/Tauhidur_Headshot.jpg",
+            role: "Assistant Professor",
+            email: "trahman@ucsd.edu",
+            affiliation: "Halıcıoğlu Data Science Institute, UCSD"
+        }
+    },
+    current_students: {
+        "Avanti Bhandarkar": {
+            img: "https://ca.slack-edge.com/TB36W6R8V-U06STPADGEL-06fa36d08fac-192",
+            role: "Graduate Student"
+        },
+        "Caden Stewart": {
+            img: "https://ca.slack-edge.com/TB36W6R8V-U07HV9YUG7R-0146167527a7-192",
+            role: "Graduate Student"
+        },
+        "Conan Minihan": {
+            img: "https://ca.slack-edge.com/TB36W6R8V-U07LS02B3AR-67cb49f57ae0-192",
+            role: "Graduate Student"
+        },
+        "Farhan": {
+            img: "https://media.licdn.com/dms/image/v2/D4E35AQE1En4ECgy3NA/profile-framedphoto-shrink_400_400/profile-framedphoto-shrink_400_400/0/1729101183653?e=1734559200&v=beta&t=qcJvqWs7EMwo6-jQYTXV13TPW0uLruPPjfGiN_MuwBc",
+            role: "Graduate Student"
+        },
+        "Forsad Al Hossain": {
+            img: "https://media.licdn.com/dms/image/v2/C4D03AQGasuImN_1yHQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1584890904613?e=1739404800&v=beta&t=CSx_I0WUz55wUROCr3TE5-CIxGgE3NLsFcTEDCWneq8",
+            role: "Graduate Student"
+        },
+        "Jiacheng Qiu": {
+            img: "https://media.licdn.com/dms/image/v2/D5603AQFSAu4BPUo5LA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1681420913897?e=1739404800&v=beta&t=O3c1FwT8KK2KU_CyRGwY8nkjfVbiUyoFkIkBKOLh4tw",
+            role: "Graduate Student",
+            social: {
+                linkedin: "https://www.linkedin.com/in/jiacheng-qiu-17361b1a0/"
+            }
+        },
+        "Yuliang Chen": {
+            img: "https://media.licdn.com/dms/image/v2/D5635AQHq1GJl8iDULQ/profile-framedphoto-shrink_400_400/profile-framedphoto-shrink_400_400/0/1691639938293?e=1734559200&v=beta&t=-B9eDUKjltH79cLRiQH9iEjLXEV6WgaUfxB6IOqaD7g",
+            role: "Graduate Student"
+        },
+        "Manasa Kalanadhabhatta": {
+            img: "https://media.licdn.com/dms/image/v2/D4D03AQEaKvhcjcP6Ew/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1728691581509?e=1739404800&v=beta&t=j4hFmtK46-n9bwnA-tipWrp94RvhzgIGDelRirjCUvU",
+            role: "Graduate Student"
+        },
+        "Masfiqur Rahaman": {
+            img: "https://media.licdn.com/dms/image/v2/D5603AQGPEsxvVCuObQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1700434209652?e=1739404800&v=beta&t=QCFZ8uZDxuyjyOAIdLaipe1GXbVskA7J9UvMwzf3R8U",
+            role: "Graduate Student"
+        },
+        "Nigel Doering": {
+            img: "https://media.licdn.com/dms/image/v2/C5603AQHOlHKb4mXTSQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1638462344739?e=1739404800&v=beta&t=5RygDpd8vvtJTbw6wdPH0pkwBwV9ghq4Z2fmCxMWToY",
+            role: "Graduate Student"
+        },
+        "Tamar Schaap": {
+            img: "https://media.licdn.com/dms/image/v2/C4E03AQFwMkyDNjcMbw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1619428016564?e=1739404800&v=beta&t=Bl3mBdCRRCECyR6UbIO6T0gfIVRvAOfLqeZuN2Xsuq0",
+            role: "Graduate Student"
+        },
+        "Tanjid Hasan Tonmoy": {
+            img: "https://media.licdn.com/dms/image/v2/D5603AQE1QNEtDE7QKw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1678138419571?e=1739404800&v=beta&t=RevT0i6eC9BJl61Ybrn0FOBueRV9kVFXFg0xrrc9csY",
+            role: "Graduate Student"
+        },
+        "Yunfei Luo": {
+            img: "https://media.licdn.com/dms/image/v2/D5603AQGj5e-YoP7zUA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1701216821032?e=1739404800&v=beta&t=eKUJSyNTC-ESwOZabKwv7elKz8GbMfqFp2wJk-oLsOo",
+            role: "Graduate Student",
+            social: {
+                linkedin: "https://www.linkedin.com/in/yunfei-luo/"
+            }
+        }
+    },
+    alumni: {
+        "Bhanu Teja Gullapalli": {
+            img: "https://media.licdn.com/dms/image/v2/C4D03AQGmsUWhk2E_8Q/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1658872757108?e=1739404800&v=beta&t=eSKbhBfnx3L8efH8vqFqvevQCOFdeky1MLfGvIm447g",
+            role: "Postdoc"
+        }
+    },
+    visiting_scholars: {
+        "Amol Harsh": {
+            img: "https://media.licdn.com/dms/image/v2/D4D03AQHwYBx_qYNfEg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1696588935237?e=1739404800&v=beta&t=g5lPLYsglxWCSyubChEdNGcw-mPFymnwktFBigznOjg",
+            role: "Undergraduate Student",
+            social: {
+                linkedin: "https://www.linkedin.com/in/amol-harsh-355523141/",
+                github: "https://github.com/AmolHarsh"
+            }
+        },
+        "Kaustubh Singh": {
+            img: "https://media.licdn.com/dms/image/v2/D4D03AQHEBgmqmY7yZg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1668332235413?e=1739404800&v=beta&t=iWLOmGhgbXoyaQ3iMgp0w7yk1GjfHIiTM9Ls9MgmFIQ",
+            role: "Undergraduate Student"
+        },
+        "Rahath Malladi": {
+            img: "https://media.licdn.com/dms/image/v2/C4D03AQGCr-KtRk5VTA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1657537182168?e=1739404800&v=beta&t=r8U4AY6hLLJk_Jecz_jerwmvLhpXuHFNnco9MCA8_BY",
+            role: "Undergraduate Student"
+        }
+    }
+};
