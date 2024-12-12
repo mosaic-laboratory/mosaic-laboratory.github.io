9338f562d5c145b3e83acc29c29af6592adf72fa
yunfeiluo <2630064129@qq.com>
2024-12-11 21:18:37 -0800

    add papers

--- data/publications.js
+++ data/publications.js
@@ -13,12 +13,122 @@ var publication_list = {
         'title': "Start Simple: Progressive Difficulty Multitask Learning",
         'authors': "Yunfei Luo, Yuyang Liu, Rukai Cai, Tauhidur Rahman",
         'venue': "NAACL Student Researcher Track 2024",
-        'year': 2022,
+        'year': 2024,
         'paper_pdf': "https://aclanthology.org/2024.naacl-srw.7/",
         "code": "https://github.com/yunfeiluo/Hierarchical-Text-Classification",
         "image": "/assets/img/publications/multinlp2024.png",
         "poster": "https://docs.google.com/presentation/d/1onm2NFIYqCFRYWmKg-CrRtVk3GyNPohE/edit?usp=sharing&ouid=109208499330994003068&rtpof=true&sd=true"
     },
+    // 2022
+    "calico_2022": {
+        "title": "Calico: Track Based Interactive and Relocatable Wearables with Fast, Reliable, and Precise Locomotion.",
+        "authors": "Anup Sathya, Jiasheng Li, Tauhidur Rahman, Ge Gao, Huaishu Peng",
+        "venue": "IMWUT 2022",
+        "year": 2022,
+        "paper_pdf": "#",
+        "image": "/assets/img/welcome-img.png"
+    },
+    "hyperspectral_image_super_resolution_2022": {
+        "title": "Hyperspectral Image Super-Resolution in Arbitrary Input-Output Band Settings.",
+        "authors": "Zhongyang Zhang, Zhiyang Xu, Zia Ahmed, Asif Salekin, Tauhidur Rahman",
+        "venue": "WACI 2022",
+        "year": 2022,
+        "paper_pdf": "#",
+        "image": "/assets/img/welcome-img.png"
+    },
+    "earlyscreen_2022": {
+        "title": "EarlyScreen: Multi-scale Instance Fusion for Predicting Neural Activation and Psychopathology in Preschool Children.",
+        "authors": "Manasa Kalanadhabhatta, Adrelys Mateo Santana, Zhongyang Zhang, Deepak Ganesan, Adam S Grabell, Tauhidur Rahman",
+        "venue": "IMWUT 2022",
+        "year": 2022,
+        "paper_pdf": "#",
+        "image": "/assets/img/welcome-img.png"
+    },
+    "prefrontal_modulation_2022": {
+        "title": "Prefrontal Modulation of Frustration-related Physiology in Preschool Children Ranging from Low to Severe Irritability.",
+        "authors": "Adam S Grabell, Adrelys Mateo Santana, Kari N Thomsen, Katie Gonzalez, Zhongyang Zhang, Zachary Bivins, Tauhidur Rahman",
+        "venue": "Developmental Cognitive Neuroscience 2022",
+        "year": 2022,
+        "paper_pdf": "#",
+        "image": "/assets/img/welcome-img.png"
+    },
+    // 2021
+    "opitrack_2021": {
+        "title": "OpiTrack: A Wearable-based Clinical Opioid Use Tracker with Temporal Convolutional Attention Networks.",
+        "authors": "Bhanu Teja Gullapalli, Stephanie Carreiro, Brittany Chapman, Deepak Ganesan, Jan Sjoquist, Tauhidur Rahman",
+        "venue": "IMWUT 2021",
+        "year": 2021,
+        "paper_pdf": "#",
+        "image": "/assets/img/welcome-img.png",
+        "poster": "#"
+    },
+    "joint_prediction_cocaine_2021": {
+        "title": "Joint Prediction of Cocaine Craving and Euphoria using Structured Prediction Energy Networks.",
+        "authors": "Bhanu Teja Gullapalli, Gustavo A Angarita, Deepak Ganesan, Tauhidur Rahman",
+        "venue": "DigiBiom 2021",
+        "year": 2021,
+        "paper_pdf": "#",
+        "image": "/assets/img/welcome-img.png",
+        "poster": "#"
+    },
+    "knowledge_transfer_2021": {
+        "title": "Knowledge Transfer across Imaging Modalities Via Simultaneous Learning of Adaptive Autoencoders for High-Fidelity Mobile Robot Vision.",
+        "authors": "Md Mahmudur Rahman, Tauhidur Rahman, Donghyun Kim, Mohammad Arif Ul Alam",
+        "venue": "IROS 2021",
+        "year": 2021,
+        "paper_pdf": "#",
+        "image": "/assets/img/welcome-img.png"
+    },
+    "effect_sleep_cognition_2021": {
+        "title": "Effect of Sleep and Biobehavioral Patterns on Multidimensional Cognitive Performance: Longitudinal, In-the-Wild Study.",
+        "authors": "Manasa Kalanadhabhatta, Tauhidur Rahman, Deepak Ganesan",
+        "venue": "JMIR 2021",
+        "year": 2021,
+        "paper_pdf": "#",
+        "image": "/assets/img/welcome-img.png"
+    },
+    // 2020
+    "mechanobeat_2020": {
+        "title": "MechanoBeat: Monitoring Interactions with Everyday Objects using 3D Printed Harmonic Oscillators and Ultra-Wideband Radar.",
+        "authors": "Md. Farhan Tasnim Oshim, Julian Killingback, Dave Follette, Huaishu Peng, Tauhidur Rahman",
+        "venue": "UIST 2020",
+        "year": 2020,
+        "paper_pdf": "#",
+        "image": "/assets/img/welcome-img.png",
+        "poster": "#"
+    },
+    "flusense_2020": {
+        "title": "FluSense: A Contactless Syndromic Surveillance Platform for Influenza-Like Illness in Hospital Waiting Areas.",
+        "authors": "Forsad Al Hossain, Andrew A. Lover, George A. Corey, Nicholas G. Reich, Tauhidur Rahman",
+        "venue": "IMWUT 2020",
+        "year": 2020,
+        "paper_pdf": "#",
+        "image": "/assets/img/welcome-img.png"
+    },
+    "mhealth_covid_2020": {
+        "title": "Can mHealth Technology Help Mitigate the Effects of the COVID-19 Pandemic?",
+        "authors": "Catherine Adans-Dester et al.",
+        "venue": "IEEE Open Journal of Engineering in Medicine and Biology 2020",
+        "year": 2020,
+        "paper_pdf": "#",
+        "image": "/assets/img/welcome-img.png"
+    },
+    "mobile_sensing_alertness_2020": {
+        "title": "Mobile Sensing of Alertness, Sleep and Circadian Rhythm: Hardware and Software Platform.",
+        "authors": "Akane Sano, Tauhidur Rahman, Mi Zhang, Deepak Ganesan, Tanzeem Choudhury",
+        "venue": "GetMobile 2020",
+        "year": 2020,
+        "paper_pdf": "#",
+        "image": "/assets/img/welcome-img.png"
+    },
+    "respiratory_rate_monitoring_2020": {
+        "title": "Respiratory Rate Monitoring in Clinical Environments with a Contactless Ultra-Wideband Impulse Radar-based Sensor System.",
+        "authors": "Goldfine*, Charlotte Oshim*, Md. Farhan Tasnim, Stephanie Carreiro, Brittany Chapman, Deepak Ganesan, Tauhidur Rahman",
+        "venue": "Hawaii International Conference on System Sciences 2020",
+        "year": 2020,
+        "paper_pdf": "#",
+        "image": "/assets/img/welcome-img.png"
+    },    
     // 2019
     'wince2019': {
         'title': "W!NCE: Unobtrusive Sensing of Upper Facial Action Units with EOG-based Eyewear.",
