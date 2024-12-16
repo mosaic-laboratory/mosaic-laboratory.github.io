b42bab28f349940801d142eb06a3173ed5a190f4
yunfeiluo <2630064129@qq.com>
2024-12-16 15:02:43 -0800

    init for project landing page

---
+++ assets/js/project_landing_page.js
@@ -0,0 +1,50 @@
+class ProjectLandingPage extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    make_online_list(){  
+        let list = [];
+
+        for (let item in project_list) {
+            list.push(
+                <div className="col-xl-4 col-md-6" data-aos="fade-up" data-aos-delay="100" key={item}>
+                    <article>
+
+                    <div className="post-img">
+                        <a href={project_list[item]["landing_page"]}><img src={project_list[item]["image"]} alt="" className="img-fluid"/></a>
+                    </div>
+
+                    <p className="post-category">{project_list[item]["tags"]}</p>
+
+                    <h2 className="title">
+                        <a href={project_list[item]["landing_page"]}>{project_list[item]["title"]}</a>
+                    </h2>
+
+                    <div className="d-flex align-items-center">
+                        {/* <img src="assets/img/blog/blog-author.jpg" alt="" className="img-fluid post-author-img flex-shrink-0"/> */}
+                        <div className="post-meta">
+                        <p className="post-author">{project_list[item]["summary"]}</p>
+                        <p className="post-date">
+                            {project_list[item]["status"]}
+                            {/* <time datetime="2022-01-01">{project_list[item]["status"]}</time> */}
+                        </p>
+                        </div>
+                    </div>
+
+                    </article>
+                </div>
+            );
+        }
+
+        return (<div className="row gy-4">{list}</div>);
+    }
+
+    // rendor funcion
+    render () {
+        return this.make_online_list();
+    }
+}
+ReactDOM.render(
+    <ProjectLandingPage></ProjectLandingPage>,
+    document.getElementById("project_list")
+);
--- data/projects.js
+++ data/projects.js
@@ -4,7 +4,7 @@ var project_list = {
         "summary": "Developing wearable and mobile systems to monitor health outcomes. Our work focuses on creating wearable devices that provide accurate, real-time health monitoring to support medical decisions.",
         "tags": "Wearable Devices | Smart Health Monitoring",
         "image": "assets/img/projects/mobile_health.png",
-        "landing_page": "services-details.html",
+        "landing_page": "project-details.html",
         "status": "On going"
     },
     'cog_health': {
@@ -12,7 +12,7 @@ var project_list = {
         "summary": "Exploring the interaction between cognitive processes and physiological signals. This theme aims to understand how behavior and brain activity influence mental health and cognitive performance.",
         "tags": "Machine Learning | Applied AI",
         "image": "assets/img/projects/cog_health.png",
-        "landing_page": "services-details.html",
+        "landing_page": "project-details.html",
         "status": "On going"
     },
     'digital_epi': {
@@ -20,7 +20,7 @@ var project_list = {
         "summary": "Researching health trends and disease outbreaks using digital biomarkers. Projects in this theme address syndromic surveillance and health monitoring in public spaces.",
         "tags": "Machine Learning | Contactless Sensing | Health Dataset",
         "image": "assets/img/projects/digital_epi.png",
-        "landing_page": "services-details.html",
+        "landing_page": "project-details.html",
         "status": "On going"
     },
     'signal_ai': {
@@ -28,7 +28,7 @@ var project_list = {
         "summary": "Leveraging AI and signal processing for ubiquitous computing challenges. This includes creating efficient algorithms to enhance image resolution and adapt models for diverse applications.",
         "tags": "Signal Processing | Deep Learning",
         "image": "assets/img/projects/signal_ai.png",
-        "landing_page": "services-details.html",
+        "landing_page": "project-details.html",
         "status": "On going"
     },
     'interact_sys': {
@@ -36,7 +36,7 @@ var project_list = {
         "summary": "Designing human-centered systems that integrate seamlessly with daily life. Our projects involve innovative technologies that enhance everyday interaction and usability.",
         "tags": "Sensors | HCI",
         "image": "assets/img/projects/interact_sys.png",
-        "landing_page": "services-details.html",
+        "landing_page": "project-details.html",
         "status": "On going"
     }
 }
