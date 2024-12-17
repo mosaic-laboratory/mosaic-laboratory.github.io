0eaf78ebd8813b4f8be29819d177a12bc25585e6
yunfeiluo <2630064129@qq.com>
2024-12-17 12:41:19 -0800

    init for project landing page

---
+++ assets/js/project_landing_page/main_title.js
@@ -0,0 +1,25 @@
+class ProjectLandingTitle extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    make_online_list(){  
+
+        // console.log(window.project_name);
+
+        return (
+            <div>
+              <h1>{project_list[window.project_name]["title"]} (Webpage under development)</h1>
+              <p className="mb-0">{project_list[window.project_name]["tags"]}</p>
+            </div>
+        );
+    }
+
+    // rendor funcion
+    render () {
+        return this.make_online_list();
+    }
+}
+ReactDOM.render(
+    <ProjectLandingTitle></ProjectLandingTitle>,
+    document.getElementById("project_landing_title")
+);
--- data/projects.js
+++ data/projects.js
@@ -1,18 +1,20 @@
 var project_list = {
     'mobile_health': {
         'title': "Mobile Health Sensing",
-        "summary": "Developing wearable and mobile systems to monitor health outcomes. Our work focuses on creating wearable devices that provide accurate, real-time health monitoring to support medical decisions.",
         "tags": "Wearable Devices | Smart Health Monitoring",
         "image": "assets/img/projects/mobile_health.png",
-        "landing_page": "project-details.html",
-        "status": "On going"
+        "landing_page": "projects-details.html?project_name=mobile_health",
+        "status": "On going",
+        "summary": "Developing wearable and mobile systems to monitor health outcomes. Our work focuses on creating wearable devices that provide accurate, real-time health monitoring to support medical decisions.",
+        "paper_list": ["opitrack_2021", "mobile_sensing_alertness_2020"],
+        "contributors": []
     },
     'cog_health': {
         'title': "Cognitive and Behavioral Health",
         "summary": "Exploring the interaction between cognitive processes and physiological signals. This theme aims to understand how behavior and brain activity influence mental health and cognitive performance.",
         "tags": "Machine Learning | Applied AI",
         "image": "assets/img/projects/cog_health.png",
-        "landing_page": "project-details.html",
+        "landing_page": "projects-details.html?project_name=cog_health",
         "status": "On going"
     },
     'digital_epi': {
@@ -20,7 +22,7 @@ var project_list = {
         "summary": "Researching health trends and disease outbreaks using digital biomarkers. Projects in this theme address syndromic surveillance and health monitoring in public spaces.",
         "tags": "Machine Learning | Contactless Sensing | Health Dataset",
         "image": "assets/img/projects/digital_epi.png",
-        "landing_page": "project-details.html",
+        "landing_page": "projects-details.html?project_name=digital_epi",
         "status": "On going"
     },
     'signal_ai': {
@@ -28,7 +30,7 @@ var project_list = {
         "summary": "Leveraging AI and signal processing for ubiquitous computing challenges. This includes creating efficient algorithms to enhance image resolution and adapt models for diverse applications.",
         "tags": "Signal Processing | Deep Learning",
         "image": "assets/img/projects/signal_ai.png",
-        "landing_page": "project-details.html",
+        "landing_page": "projects-details.html?project_name=signal_ai",
         "status": "On going"
     },
     'interact_sys': {
@@ -36,7 +38,7 @@ var project_list = {
         "summary": "Designing human-centered systems that integrate seamlessly with daily life. Our projects involve innovative technologies that enhance everyday interaction and usability.",
         "tags": "Sensors | HCI",
         "image": "assets/img/projects/interact_sys.png",
-        "landing_page": "project-details.html",
+        "landing_page": "projects-details.html?project_name=interact_sys",
         "status": "On going"
     }
 }
